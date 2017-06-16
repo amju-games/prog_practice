6862ce263db0513d342969341d0d927793b4d646
Jason Colman <jason@amju.com>
2017-06-16 10:00:25 +0100

    TODO list and (barely started) linked list

---
+++ linked_lists/linked_lists.cpp
@@ -0,0 +1,23 @@
+// j.c. programming practice
+// Linked list
+
+class list
+{
+public:
+
+
+private:
+
+  struct list_node
+  {
+    int payload = 0;
+    list_node* next = nullptr;
+  };
+
+  
+};
+
+
+int main()
+{
+}
